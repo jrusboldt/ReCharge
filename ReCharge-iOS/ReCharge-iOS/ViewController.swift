d5fd86876f977ecd63e15b5c59092d7d04b527dc
jbou47 <jboodrow@yahoo.com>
2019-01-31 21:03:49 -0500

    initial iOS build

---
+++ ReCharge-iOS/ReCharge-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReCharge-iOS
+//
+//  Created by Justin Boudreau on 1/31/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
