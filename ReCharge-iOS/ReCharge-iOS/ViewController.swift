d32ed0f3d52017f5c4c0eb926fcaca4fe871e58b
jbou47 <jboodrow@yahoo.com>
2019-02-06 10:21:23 -0500

    basic map implementation with location services

--- ReCharge-iOS/ReCharge-iOS/ViewController.swift
+++ ReCharge-iOS/ReCharge-iOS/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        checkLocationServices()
     }
 
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
 
+    //check system wide location services
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        }
+        else {
+            // show alert to enable location services
+        }
+    }
+    
+    //checks app specific location services
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            //show alert to turn on location services in settings
+            break
+        case .notDetermined:
+            //prompt user to allow location services when app in use
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //location services are unavialable due to parental controls
+            //show alert
+            break
+        case .authorizedAlways:
+            break
+        default:
+            break
+        }
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //
+    }
 }
 
