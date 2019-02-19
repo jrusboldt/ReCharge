d7c3bf90a4c63a32bcbd1bf130bdab7e16bef14f
jbou47 <jboodrow@yahoo.com>
2019-02-18 22:58:39 -0500

    Update FuelStation annotation object and hardcoded in WL stations

--- ReCharge-iOS/ReCharge-iOS/FuelStations.swift
+++ ReCharge-iOS/ReCharge-iOS/FuelStations.swift
@@ -6,48 +6,61 @@
 //
 
 
+
 import MapKit
 
-class FuelStations: NSObject, MKAnnotation {
+class FuelStationAnnotation: NSObject, MKAnnotation {
   // instance variables
   let title : String?
-  let station_name : String
-  let station_phone : String
-  let latitude : Double
-  let longitude : Double
-  let city : String
-  let intersection_directions : String
-  let state : String
-  let street_address : String
-  let zip : String
-  let coordinate : CLLocationCoordinate2D
-  let is_parking_avaiable : Bool
-  let is_charging_avaiavle : Bool
+  var station_name : String
+  //var station_phone : String
+  var latitude : Double
+  var longitude : Double
+  //var city : String
+  //var intersection_directions : String
+  //var state : String
+  //var street_address : String
+  //var zip : String
+  var coordinate : CLLocationCoordinate2D
+  var is_parking_avaiable : Bool
+  var is_charging_avaiavle : Bool
 
-  init(JSONString: String) {
-    super.init()
+    init(station_name: String, latitude: Double, longitude: Double) {
+        self.title = station_name
+        self.station_name = station_name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        self.is_parking_avaiable = true
+        self.is_charging_avaiavle = true
+        
+        super.init()
 
-    var error : NSError?
-    let JSONData = JSONString.data(using: String.Encoding.utf8, allowLossyConversion: false)
 
-    let JSONDictionary: Dictionary = JSONSerialization.JSONObjectWithData(JSONData, options: nil) as NSDictionary
+        /*
+        var error : NSError?
+        let JSONData = JSONString.data(using: String.Encoding.utf8, allowLossyConversion: false)
 
-    // Loop
-    for (key, value) in JSONDictionary {
-        let keyName = key as String
-        let keyValue: String = value as String
+        //let JSONDictionary: Dictionary = JSONSerialization.JSONObjectWithData(JSONData, options: nil) as NSDictionary
 
-        // If property exists
-        if (self.respondsToSelector(NSSelectorFromString(keyName))) {
-            self.setValue(keyValue, forKey: keyName)
+        // Loop
+        for (key, value) in JSONDictionary {
+            let keyName = key as String
+            let keyValue: String = value as String
+
+            // If property exists
+            if (self.respondsToSelector(NSSelectorFromString(keyName))) {
+                self.setValue(keyValue, forKey: keyName)
+            }
         }
-    }
 
-    self.title = station_name
-    self.coordinate = CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
-
+        self.title = station_name
+        self.coordinate = CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+    */
   }
 
   //init()
 
 }
+
