a007b798330a156bf1dff3d9aec0f090e3327502
jbou47 <jboodrow@yahoo.com>
2019-02-19 00:04:05 -0500

    Initial settings layout

---
+++ ReCharge-iOS/ReCharge-iOS/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  ReCharge-iOS
+//
+//  Created by Justin Boudreau on 2/18/19.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
