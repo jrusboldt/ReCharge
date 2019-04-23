772e77addf0aaa2c20bf7e614cd6f820730b5211
Ramsey <ali59@purdue.edu>
2019-04-22 20:46:48 -0400

    *added "live" data emulation to server

--- ReCharge-Server/server.js
+++ ReCharge-Server/server.js
@@ -30,6 +30,8 @@ connection.connect(function(err) {
   console.log('Connected to database.');
 });
 
+setInterval(emulateSensor, 10000);
+
 var  executeQuery = function(res, query){
   connection.query(query, function (error, results, fields) {
 		if (error) {
@@ -65,3 +67,56 @@ app.get("/api/status/all", function(req , res){
                 var query = "SELECT * FROM main.HISTORY WHERE ID =" + req.body.ID;
                 executeQuery(res, query);
 });
+
+function emulateSensor() {
+    var parkingSpaceID = 76232;
+    var firstParkingSpace = Math.floor(Math.random()*2);
+    var secondParkingSpace = Math.floor(Math.random()*2);
+    var remainUnchanged = Math.floor(Math.random()*4);
+    var usingCharger = 0;
+
+    if (usingCharger == 0 && firstParkingSpace == 1) {
+      usingCharger = Math.floor(Math.random()*2);
+    }
+
+    if (usingCharger == 0 && secondParkingSpace == 1) {
+      usingCharger = Math.floor(Math.random()*2);
+    }
+
+    if (usingCharger == 1) {
+      usingCharger = "Y";
+    } else {
+      usingCharger = "N";
+    }
+
+    //ex.
+    //parkingSpaceID = 76232;
+    //usingCharger = Y;
+    //firstParkingSpace = 0
+    //secondParkingSPace = 1
+    //remainUnchanged = 0
+
+    var insertQuery = "INSERT INTO main.HISTORY (ID, TIMESTAMP, AVAILABILITY) VALUES (" + parkingSpaceID + ", CURRENT_TIMESTAMP , CASE WHEN (SELECT AVAILABLE FROM main.STATUS WHERE ID = " + parkingSpaceID + ") = \"Y\" THEN \"AVAILABLE\" ELSE \"IN-USE\" END )";
+    var updateQuery = "UPDATE main.STATUS SET AVAILABLE = \"" + usingCharger + "\", REMAINING_SPACE = " + (firstParkingSpace + secondParkingSpace) + " WHERE ID = " + parkingSpaceID;
+
+    connection.query(insertQuery, function (error, results, fields) {
+      if (error) {
+        console.log("EMULATION INSERTION ERROR: " + error);
+      } else {
+        console.log("EMULATION INSERTION SUCCESS");
+        if (remainUnchanged == 0) {
+          console.log("EMULATION UPDATE SUCCESS | (SKIPPED)");
+          console.log("Waiting...");
+        } else {
+          connection.query(updateQuery, function (error, results, fields) {
+            if (error) {
+              console.log("EMULATION UPDATE ERROR: " + error);
+            } else {
+              console.log("EMULATION UPDATE SUCCESS | Using Charger: " + usingCharger + " | Parking Spaces: " + (firstParkingSpace + secondParkingSpace));
+              console.log("Waiting...");
+            }
+          });
+        }
+      }
+    });
+}
